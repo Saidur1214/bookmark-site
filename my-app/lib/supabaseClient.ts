e475cb43ca1bb5ecbd35a233bc44e65acaa54f6d
saidur <1214+saidur@said.noreply.github.com>
2026-09-10 13:30:19 +0600

    fix 1

--- my-app/lib/supabaseClient.ts
+++ my-app/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
-import type { Database } from "../types/db.types";
 import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "../types/database.types";
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
